d49ef2a4dac64139fc0f798eb35d6854e5bfc3b7
Magic <artemiypro547547@gmail.com>
2025-02-25 21:29:45 +0300

    magical test

---
+++ main,py/main.py
@@ -0,0 +1,4 @@
+requestValue1 = int(input("Enter the first meaning"))
+requestValue2 = int(input("Enter the second meaning"))
+resultValue = requestValue1 ** requestValue2
+print(f"Result of your meanings {resultValue}")
